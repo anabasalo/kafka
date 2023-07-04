536dac8b99aeee80bedfc25eb2ac834b651f50e1
Ana Basalo <ana.basalo@loggi.com>
2023-07-03 21:09:36 -0300

    adding interceptor
    Signed-off-by: Ana Basalo <ana.basalo@loggi.com>

--- str-consumer/src/main/java/com/ana/strconsumer/config/StringConsumerConfig.java
+++ str-consumer/src/main/java/com/ana/strconsumer/config/StringConsumerConfig.java
@@ -1,6 +1,8 @@
 package com.ana.strconsumer.config;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
@@ -9,9 +11,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.RecordInterceptor;
 
 import java.util.HashMap;
 
+@Log4j2
 @RequiredArgsConstructor
 @Configuration
 public class StringConsumerConfig {
@@ -29,11 +33,27 @@ public class StringConsumerConfig {
 
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, String> strContainerFactory(
-            ConsumerFactory<String, String> consumerFactory
-    ) {
+            ConsumerFactory<String, String> consumerFactory) {
         var factory = new ConcurrentKafkaListenerContainerFactory<String, String>();
         factory.setConsumerFactory(consumerFactory);
         return factory;
     }
 
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> validMessageContainerFactory(
+            ConsumerFactory<String, String> consumerFactory) {
+        var factory = new ConcurrentKafkaListenerContainerFactory<String, String>();
+        factory.setConsumerFactory(consumerFactory);
+        factory.setRecordInterceptor(validMessage());
+        return factory;
+    }
+
+    private RecordInterceptor<String, String> validMessage() {
+        return record -> {
+            if (record.value().contains("Teste")) {
+                log.info("Posssui a palavra Teste");
+            }
+            return record;
+        };
+    }
 }
--- str-consumer/src/main/java/com/ana/strconsumer/listeners/StrConsumerListener.java
+++ str-consumer/src/main/java/com/ana/strconsumer/listeners/StrConsumerListener.java
@@ -1,6 +1,8 @@
 package com.ana.strconsumer.listeners;
 
 import lombok.extern.log4j.Log4j2;
+
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 import com.ana.strconsumer.custom.StrConsumerCustomListener;
@@ -19,7 +21,7 @@ public class StrConsumerListener {
         log.info("LOG ::: Receive message {}", message);
     }
 
-    @StrConsumerCustomListener(groupId = "group-2")
+    @KafkaListener(groupId = "group-2", topics = "str-topic", containerFactory = "validMessageContainerFactory")
     public void history(String message) {
         log.info("HISTORY ::: Receive message {}", message);
     }
